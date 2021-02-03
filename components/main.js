f02dfdf9e1e5c1248bacae1426d9085cdf8edcd8
Md Sabbir Hossain <m.sabbirhossain308@gmail.com>
2021-02-03 08:27:20 +0600

    component

--- components/main.js
+++ components/main.js
@@ -1,5 +1,23 @@
 Vue.component('task', {
-	template: '<li>Foobar</li>'
+	template: '<li><slot></slot></li>'
+})
+
+Vue.component('task-list', {
+	template: '<div><task v-for="task in tasks">{{ task.description }}</task></div>',
+
+	data(){
+		return{
+			tasks: [
+				{ description: "Go to the Store", completed: true},
+				{ description: "Hello World", completed: false},
+				{ description: "Revange for nature", completed: false},
+				{ description: "GNothing to do", completed: true},
+				{ description: "Go to the Store32", completed: false},
+				{ description: "Go to the Store43543", completed: true},
+				{ description: "Go to the Store44", completed: false},
+			]
+		}
+	}
 })
 
 var app = new Vue({
