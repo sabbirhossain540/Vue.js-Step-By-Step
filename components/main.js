c3e55130f27b5106c32afab610444e3a30b6c22b
Md Sabbir Hossain <m.sabbirhossain308@gmail.com>
2021-02-03 07:44:24 +0600

    vue component

---
+++ components/main.js
@@ -0,0 +1,20 @@
+Vue.component('task', {
+	template: '<li>Foobar</li>'
+})
+
+var app = new Vue({
+			el: '#root',
+			data:{
+
+			},
+
+			methods: {
+
+			},
+
+			mounted(){
+
+			}
+			
+
+		})
